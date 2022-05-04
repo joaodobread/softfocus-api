5560fb92ca592ce6f019199177fe3e4efd55a9d0
João Victor <joaovictorslf@gmail.com>
2022-05-04 19:41:30 -0300

    add database mapping to class

---
+++ src/sqlalchemy/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from decouple import config
+
+SQLALCHEMY_DATABASE_URL = config('DATABASE_CONNECTION')
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    except:
+        db.close()
---
+++ src/sqlalchemy/models.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from sqlalchemy import ForeignKey, Integer, String, DateTime, Boolean, Enum
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.orm import relationship
+from geoalchemy2 import Geometry
+
+from .database import Base
+
+
+class Analysts(Base):
+    __tablename__ = 'analysts'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(300), nullable=False)
+    email = Column(String(300), nullable=False)
+    password = Column(String(300), nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    is_super = Column(Boolean, nullable=False, default=False)
+    loss_communication = relationship("LossCommunication", lazy='joined')
+
+
+class LossCommunication(Base):
+    __tablename__ = 'loss_communication'
+    id = Column(Integer, primary_key=True)
+    analysts_id = Column(Integer, ForeignKey('analysts.id'))
+    analyst = relationship("Analysts", back_populates="loss_communication")
+    farmer_name = Column(String(300), nullable=False)
+    farmer_email = Column(String(300), nullable=False)
+    farmer_document = Column(String(20), nullable=False)
+    location = Column(Geometry(geometry_type='POINT',
+                      srid=4326), nullable=False)
+    harvest_date = Column(DateTime, nullable=False, default=datetime.utcnow)
+    couse_of_loss = Column(
+        Enum('excessive_rain', 'frost', 'hail', 'dry', 'gale', 'ray'), nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    deleted = Column(Boolean, nullable=True, default=False)
